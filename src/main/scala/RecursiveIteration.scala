23186ea16dadd3839c1e24a93c7ef3b974f765b0
Michał Przybylak <michal.przybylak@gmail.com>
2017-11-25 23:57:15 +0100

    comments for recursive iterations

--- src/main/scala/RecursiveIteration.scala
+++ src/main/scala/RecursiveIteration.scala
@@ -3,15 +3,26 @@ import scala.annotation.tailrec
 object RecursiveIteration {
 
   def factorial(n: Int): Int = {
+
     @tailrec
+    /**
+      * this is a classic way of expressing looping algorithms
+      * as recursion in scala - we will define internal function
+      * that will take one additional argument - an accumulator
+      * accumulator is used to pass information between the recursive
+      * calls - in this case intermediate result of calculating factorial
+      */
     def factorial(n: Int, acc: Int = 1): Int = {
-      if (n <= 0) acc else factorial(n - 1, n * acc)
+      if (n <= 0) acc else factorial(n - 1, n * acc) // passing intermediate result
     }
 
+    // starting computation
     factorial(n)
   }
 
   def fibonacci(n: Int): Int = {
+
+    // adding @tailrec annotation forces compiler to fail if this is NOT tail-recursive method
     @tailrec
     def fib(n: Int, a: Int = 0, b: Int = 1): Int = {
       if (n < 1) a else fib(n - 1, a + b, a)
