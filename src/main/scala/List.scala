748941dfcd7f51ff52e2a105065dca62ff2e92d1
Michał Przybylak <michal.przybylak@gmail.com>
2017-11-26 00:01:35 +0100

    comments for list definition

--- src/main/scala/List.scala
+++ src/main/scala/List.scala
@@ -1,3 +1,18 @@
+
+/**
+  * Sealed means that this class can be extend only inside this file
+  *
+  * +A in type declaration means that A is covariant, so
+  * if Bar is subtype of Foo than List[Bar] is subtype of List[Foo]
+  */
 sealed trait List[+A]
+
+/**
+  * Case objects can take part in pattern matching
+  *
+  * Nothing is considered a subtype of any other type so it is valid to say:
+  * val list: List[Int] = Nothing
+  */
 case object Nil extends List[Nothing]
+
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
