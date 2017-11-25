07f3ffb805acca576fb74dec1de21598781eeaf5
Michał Przybylak <michal.przybylak@gmail.com>
2017-11-25 19:37:44 +0100

    tailrec annotation

--- src/main/scala/RecursiveIteration.scala
+++ src/main/scala/RecursiveIteration.scala
@@ -1,6 +1,9 @@
+import scala.annotation.tailrec
+
 object RecursiveIteration {
 
   def factorial(n: Int): Int = {
+    @tailrec
     def factorial(n: Int, acc: Int): Int = {
       if(n <= 0) acc else factorial(n-1, n * acc)
     }
