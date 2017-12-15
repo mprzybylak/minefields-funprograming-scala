efda2569b07e9063b7aea82be67aa3f279a753aa
Michał Przybylak <michal.przybylak@gmail.com>
2017-12-15 18:02:02 +0100

    size and max for tree

--- src/main/scala/Tree.scala
+++ src/main/scala/Tree.scala
@@ -1,3 +1,17 @@
 sealed trait Tree[+A]
 case class Leaf[A](value: A) extends Tree[A]
 case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+
+object Tree {
+
+  def treeSize[A](tree: Tree[A]): Int = tree match {
+    case Leaf(_) => 1
+    case Branch(left, right) => treeSize(left) + treeSize(right) + 1
+  }
+
+  def treeMax(tree: Tree[Int]): Int = tree match {
+    case Leaf(v) => v
+    case Branch(left, right) => treeMax(left).max(treeMax(right))
+  }
+
+}
--- src/test/scala/TreeTest.scala
+++ src/test/scala/TreeTest.scala
@@ -1,3 +1,77 @@
-class TreeTest {
+import org.scalatest.{Matchers, WordSpec}
+
+import Tree._
+
+class TreeTest extends WordSpec with Matchers {
+
+  "a tree size" should {
+
+    "be equal to 1 for leaf" in {
+
+      // given
+      val tree = Leaf(10)
+
+      // when
+      val sizeOfTree = treeSize(tree)
+
+      // then
+      sizeOfTree shouldEqual 1
+    }
+
+    "be equal to 3 for one branch with leafs" in {
+
+      // given
+      val tree = Branch(Leaf(1), Leaf(2))
+
+      // when
+      val sizeOfTree = treeSize(tree)
+
+      // then
+      sizeOfTree shouldEqual 3
+    }
+
+    "be equal to 7 for full 3 levels tree" in {
+
+      // given
+      val tree = Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(3), Leaf(4)))
+
+      // when
+      val sizeOfTree = treeSize(tree)
+
+      // then
+      sizeOfTree shouldEqual 7
+    }
+  }
+
+  "a tree maximum" should {
+
+    "equals 1 for leaf with value 1" in {
+
+      // given
+      val tree = Leaf(1)
+
+      // when
+      val maxiumOfTree = treeMax(tree)
+
+      // then
+      maxiumOfTree shouldEqual 1
+    }
+
+    "equals 2 for branch with leaf equal to 1 and 2" in {
+
+      // given
+      val tree = Branch(Leaf(1), Leaf(2))
+
+      // when
+      val maxiumOfTree = treeMax(tree)
+
+      //then
+      maxiumOfTree shouldEqual 2
+    }
+
+
+
+  }
+
 
 }
