8e2f061e5ced9e67a2c64da044826620cffa138e
Michał Przybylak <michal.przybylak@gmail.com>
2017-12-26 00:21:52 +0100

    flatMap for either

--- src/main/scala/com/stackframelayout/minefields/funprog/errors/Either.scala
+++ src/main/scala/com/stackframelayout/minefields/funprog/errors/Either.scala
@@ -2,12 +2,15 @@ package com.stackframelayout.minefields.funprog.errors
 
 sealed trait Either[+E, +A] {
   def map[B](f: A => B): Either[E, B]
+  def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B]
 }
 
-case class Left[+E](error: E) extends Either[E, Nothing] {
+case class Left[+E](value: E) extends Either[E, Nothing] {
   override def map[B](f: Nothing => B): Left[E] = this
+  override def flatMap[EE >: E, B](f: Nothing => Either[EE, B]): Left[E] = this
 }
 
 case class Right[+A](value: A) extends Either[Nothing, A] {
   override def map[B](f: A => B): Right[B] = Right(f(value))
+  override def flatMap[EE >: Nothing, B](f: A => Either[EE, B]) = f(value)
 }
--- src/test/scala/com/stackframelayout/minefields/funprog/errors/RightTest.scala
+++ src/test/scala/com/stackframelayout/minefields/funprog/errors/RightTest.scala
@@ -30,4 +30,31 @@ class RightTest extends WordSpec with Matchers {
       newEither shouldEqual Right(11)
     }
   }
+
+  "flat map" should {
+
+    "map Left to Left" in {
+
+      // given
+      val either:Either[String, Int] = Left("error")
+
+      // when
+      val newEither = either.flatMap(v => Right(v + 1))
+
+      // then
+      newEither shouldEqual Left("error")
+    }
+
+    "map Right value using given function" in {
+
+      // given
+      val either:Either[String, Int] = Right(10)
+
+      // when
+      val newEither = either.flatMap(v => Right(v + 1))
+
+      // then
+      newEither shouldEqual Right(11)
+    }
+  }
 }
