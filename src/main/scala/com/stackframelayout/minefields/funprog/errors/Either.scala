df1264202e32547802e1cd9beea48c9f34297a00
Michał Przybylak <michal.przybylak@gmail.com>
2017-12-26 00:31:09 +0100

    orElse for either

--- src/main/scala/com/stackframelayout/minefields/funprog/errors/Either.scala
+++ src/main/scala/com/stackframelayout/minefields/funprog/errors/Either.scala
@@ -3,14 +3,17 @@ package com.stackframelayout.minefields.funprog.errors
 sealed trait Either[+E, +A] {
   def map[B](f: A => B): Either[E, B]
   def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B]
+  def orElse[EE >:E, B >: A](b: => Either[EE, B]): Either[EE, B]
 }
 
 case class Left[+E](value: E) extends Either[E, Nothing] {
   override def map[B](f: Nothing => B): Left[E] = this
   override def flatMap[EE >: E, B](f: Nothing => Either[EE, B]): Left[E] = this
+  override def orElse[EE >: E, B >: Nothing](b: => Either[EE, B]): Either[EE, B] = b
 }
 
 case class Right[+A](value: A) extends Either[Nothing, A] {
   override def map[B](f: A => B): Right[B] = Right(f(value))
   override def flatMap[EE >: Nothing, B](f: A => Either[EE, B]) = f(value)
+  override def orElse[EE >: Nothing, B >: A](b: => Either[EE, B]): Right[A] = this
 }
--- src/test/scala/com/stackframelayout/minefields/funprog/errors/RightTest.scala
+++ src/test/scala/com/stackframelayout/minefields/funprog/errors/RightTest.scala
@@ -57,4 +57,43 @@ class RightTest extends WordSpec with Matchers {
       newEither shouldEqual Right(11)
     }
   }
+
+  "orElse" should {
+
+    "return new Either in case of Left" in {
+
+      // given
+      val either: Either[String, Int] = Left("error")
+
+      // when
+      val newEither = either.orElse(Right("a"))
+
+      // then
+      newEither shouldEqual Right("a")
+    }
+
+    "return new Left in case of Left" in {
+
+      // given
+      val either: Either[String, Int] = Left("error")
+
+      // when
+      val newEither = either.orElse(Left("super error"))
+
+      // then
+      newEither shouldEqual Left("super error")
+    }
+
+    "return old Either in case of Right" in {
+
+      // given
+      val either: Either[String, Int] = Right(10)
+
+      // when
+      val newEither = either.orElse(Right(100))
+
+      // then
+      newEither shouldEqual Right(10)
+    }
+  }
 }
